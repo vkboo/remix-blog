68fe6a06a6a62c920b42e42a8328b06e7a8489c2
zhaobo <vkbo@qq.com>
2024-02-04 23:30:08 +0800

    feat: add basic posts.new.tsx

--- app/routes/posts.new.tsx
+++ app/routes/posts.new.tsx
@@ -1,7 +1,17 @@
-export default function Page() {
+import { Button, Input, Textarea } from "@nextui-org/react";
+
+export default function App() {
     return (
         <div>
-            New post
+            <div className="flex flex-col gap-3 p-12">
+                <h1 className="text-xl font-black">发布文章</h1>
+                <Input name="slug" label="slug" />
+                <Input label="文章标题" />
+                <Textarea label="内容" />
+                <Button>
+                    发布
+                </Button>
+            </div>
         </div>
-    )
+    );
 }
