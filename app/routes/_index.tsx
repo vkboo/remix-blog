6bbd5f38c7e27fc9640a8a65897e388e7e6c8c38
zhaobo <vkbo@qq.com>
2024-02-05 23:20:03 +0800

    feat: index pagination

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,65 @@
 import { LoaderFunctionArgs, json } from '@remix-run/node';
+import { useLoaderData, Link, useSearchParams } from '@remix-run/react';
+import { Pagination } from '@nextui-org/react';
 import { prisma } from '~/prisma.server';
 
+const PAGE_SIZE = 1
+
 export const loader = async (c: LoaderFunctionArgs) => {
-  const posts = await prisma.post.findMany({
-    orderBy: {
-      created_at: "desc"
-    }
-  })
+  const search = new URL(c.request.url).searchParams
+  const page = Number(search.get('page') || 1)
+
+  const [posts, total] = await prisma.$transaction([
+    prisma.post.findMany({
+      orderBy: {
+        created_at: "desc"
+      },
+      // 分页查询
+      take: PAGE_SIZE,
+      skip: (page - 1) * PAGE_SIZE
+    }),
+    prisma.post.count()
+  ])
+
 
   return json({
-    posts
+    posts,
+    pageCount: Math.ceil(total / PAGE_SIZE)
   })
+}
+
+export default function Index() {
+
+  const loaderData = useLoaderData<typeof loader>();
+  const [searchParams, setSearchParams] = useSearchParams()
+  const page = Number(searchParams.get('page') || 1)
+
+  return (
+    <div className="p-12 space-y-4">
+      <div className="flex flex-col gap-4">
+        {loaderData.posts.map(post => {
+          return (
+            <div key={post.id}>
+              <Link to={`/posts/${post.id}`} className="text-xl">
+                {post.title}
+              </Link>
+              <div className="text-sm text-gray-400">
+                {post.created_at}
+              </div>
+            </div>
+          )
+        })}
+      </div>
+
+      <Pagination
+        page={page}
+        total={loaderData.pageCount}
+        onChange={page => {
+          const newSearchParams = new URLSearchParams(searchParams)
+          newSearchParams.set('page', String(page))
+          setSearchParams(newSearchParams)
+        }}
+      />
+    </div>
+  );
 }
