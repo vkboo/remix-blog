c0fe0ff7ddbc17923a698346010a302e70929336
bo.zhao <bo.zhao@iglooinsure.com>
2024-02-05 09:49:12 +0800

    feat: basic create data into db

--- app/routes/posts.new.tsx
+++ app/routes/posts.new.tsx
@@ -1,17 +1,38 @@
 import { Button, Input, Textarea } from "@nextui-org/react";
+import { Form } from "@remix-run/react";
+import { ActionFunctionArgs, redirect } from "@remix-run/node";
+import { prisma } from '~/prisma.server';
 
-export default function App() {
+export const action = async (c: ActionFunctionArgs) => {
+    const formData = await c.request.formData();
+    const slug = formData.get('slug') as string;
+    const title = formData.get('title') as string;
+    const content = formData.get('content') as string;
+    prisma.post.create({
+        data: {
+            id: slug,
+            title,
+            content
+        }
+    });
+
+    return redirect('/');
+}
+
+export default function Page() {
     return (
         <div>
-            <div className="flex flex-col gap-3 p-12">
-                <h1 className="text-xl font-black">发布文章</h1>
-                <Input name="slug" label="slug" />
-                <Input label="文章标题" />
-                <Textarea label="内容" />
-                <Button>
-                    发布
-                </Button>
-            </div>
+            <Form method="POST">
+                <div className="flex flex-col gap-3 p-12">
+                    <h1 className="text-xl font-black">发布文章</h1>
+                    <Input name="slug" label="slug" />
+                    <Input name="title" label="文章标题" />
+                    <Textarea name="content" label="内容" />
+                    <Button type="submit" color="primary">
+                        发布
+                    </Button>
+                </div>
+            </Form>
         </div>
-    );
+    )
 }
